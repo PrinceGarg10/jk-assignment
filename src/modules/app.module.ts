849e083b730789913b777a11a8bb85d236673597
PrinceGarg10 <princegarg8198@gmail.com>
2025-01-06 17:18:15 +0530

    - serverStatic module init and make uploads folder public to see the file

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -2,10 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
 import { CommonModule } from './common';
+import { join } from 'path';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { GeneralModule } from './general/general.module';
 
 @Module({
     imports: [
+      ServeStaticModule.forRoot({
+      rootPath: join(process.cwd(), 'uploads'),
+      serveRoot: '/uploads',
+    }),
       CommonModule,
       GeneralModule
     ],
