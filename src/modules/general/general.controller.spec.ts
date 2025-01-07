d189283998feffa4a0c1577bb3b0b4bf7b24bce5
PrinceGarg10 <princegarg8198@gmail.com>
2025-01-07 17:13:49 +0530

    - General controller test run without error

--- src/modules/general/general.controller.spec.ts
+++ src/modules/general/general.controller.spec.ts
@@ -1,12 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { GeneralController } from './general.controller';
+import { GeneralService } from './general.service';
+import { FileSystemService } from '../shared/file-system.service';
 
 describe('GeneralController', () => {
   let controller: GeneralController;
 
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [GeneralController],
+      providers: [
+        {
+          provide: GeneralService,
+          useValue: GeneralService,
+        },
+        {
+          provide: FileSystemService,
+          useValue: FileSystemService,
+        },
+      ],
     }).compile();
 
     controller = module.get<GeneralController>(GeneralController);
