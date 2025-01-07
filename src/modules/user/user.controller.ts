d4b61c53e0c8bed79e7b4843c0cc94f3525f81ae
PrinceGarg10 <princegarg8198@gmail.com>
2025-01-07 12:22:13 +0530

    - implement authorization for user controller

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -3,6 +3,8 @@ import { ApiTags } from '@nestjs/swagger';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { Authorized } from '../decorators/authorized.decorator';
+import { RoleEnum } from '../common/constants/role';
 
 @Controller('user')
 @ApiTags("user")
@@ -11,29 +13,33 @@ export class UserController {
         private userService: UserService
     ) { }
 
+    @Authorized(RoleEnum.ADMIN)
     @Post()
     async createBySysadmin(@Body() createUserDto: CreateUserDto) {
         return await this.userService.create(createUserDto);
     }
 
 
+    @Authorized(RoleEnum.ADMIN)
     @Get('all')
     async findAll(@Query() query: any) {
         return await this.userService.findAll(query);
     }
 
 
+    @Authorized(RoleEnum.ADMIN)
     @Get()
     async findOne(@Query('id') id: number) {
         return await this.userService.findOne(id);
     }
 
+    @Authorized(RoleEnum.ADMIN)
     @Patch()
     async update(@Body() updateUserDto: UpdateUserDto) {
         return await this.userService.update(updateUserDto);
     }
 
-
+    @Authorized(RoleEnum.ADMIN)
     @Delete()
     async remove(@Query('id') id: string) {
         return await this.userService.remove(id);
